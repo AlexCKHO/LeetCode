65377d905eaf1bf30c2f1e4c6996c1e5ee6252cb
Alex Ho <cho@spartaglobal.com>
2023-06-08 12:25:08 +0100

    Completed TwoSum

---
+++ TwoSum/TwoSum/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 2, 7, 11, 15 };
+            Console.WriteLine(TwoSum(ints, 13)[0] + "," +TwoSum(ints, 13)[1]);
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var pairs = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+                if (pairs.ContainsKey(target - nums[i]))
+                    return new int[] { pairs[target - nums[i]], i };
+                //pair[target - nums[i]] = the previous value in the array
+
+                else
+                    pairs.TryAdd(nums[i], i);
+
+            return default;
+        }
+    }
+}
