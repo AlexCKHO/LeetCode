a8d7ac20b8f061f421ffb3f371847e5ae9724c7e
Alex Ho <cho@spartaglobal.com>
2023-07-26 13:42:25 +0100

    Completed Reverse Word

---
+++ Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Best_Time_to_Buy_and_Sell_Stock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Dictionary<string, int>  test = new Dictionary<string, int>();
+
+            test["one"] = 1;
+            test["two"] = 2;
+            test["three"] = 3;
+            test["four"] = 4;
+
+
+            foreach (KeyValuePair<string,int> pair in test)
+            {
+              
+                Console.WriteLine($"{pair.Key} is {pair.Value}"   );
+            }
+
+            foreach (KeyValuePair<string, int> pair in test)
+            {
+                if (pair.Key == "four")
+                {
+                    test.Remove("four");
+                }
+                   
+            }
+
+            foreach (KeyValuePair<string, int> pair in test)
+            {
+
+                Console.WriteLine($"{pair.Key} is {pair.Value}");
+            }
+        }
+    }
+}
---
+++ ReverseWords/ReverseWords/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace ReverseWords
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string s = "  hello world  ";
+
+
+            Console.WriteLine(s);
+
+
+        }
+
+        public static string Re(string s)
+        {
+            return String.Join(" ", s.Split(' ', StringSplitOptions.RemoveEmptyEntries).Reverse());
+            // Split will add string to array and split by ' ', 
+
+            // StringSplitOptions.RemoveEmptyEntries will remove empty ""
+            // 
+            //"apple,,banana,orange,," > Split() > ["apple", "", "banana", "orange", "", ""]
+            // StringSplitOptions.RemoveEmptyEntries will remove all the "" > ["apple", "banana", "orange"]
+
+        }
+
+
+        public static string ReverseWords(string s)
+        {
+            List<string> words = new List<string>();
+
+            string a = s.Trim();
+
+            string save = "";
+
+            string ans = "";
+
+
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (!a[i].Equals(' '))
+                {
+                    save += a[i];
+                }
+
+                if ((a[i].Equals(' ') && !String.IsNullOrEmpty(save)) || i == a.Length - 1)
+                {
+                    words.Add(save);
+                    save = "";
+                }
+
+            }
+
+            for (int j = words.Count - 1; j >= 0; j--)
+            {
+
+                ans += words[j];
+
+                if (j != 0)
+                {
+                    ans += " ";
+                }
+            }
+
+
+
+            return ans;
+
+
+        }
+
+    }
+}
