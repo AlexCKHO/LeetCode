d34acb5f1085167581b7517f05a18e7f4bdd9a3b
Alex Ho <cho@spartaglobal.com>
2023-08-23 20:44:35 +0100

    Add Encode and Decode Strings

---
+++ Encode and Decode Strings/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace Encode_and_Decode_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <string> list = new List<string>() { "", "vn" };
+            var result = decode(encode(list));
+            foreach(var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        public static string encode(IList<string> strs)
+        {
+            StringBuilder sb = new();
+            for (int i = 0; i < strs.Count; i++)
+            {
+                if (i == strs.Count - 1) //If that string is the last one in the list  then, don't add the sb.Append("\n");
+                {
+                    sb.Append(strs[i]);
+                }
+                else
+                {
+                    sb.Append(strs[i]);
+                    sb.Append("\n");
+                }
+
+            }
+            return sb.ToString();
+
+        }
+
+        // Decodes a single string to a list of strings.
+        public static IList<string> decode(string s)
+        {
+            string[] arr = s.Split('\n');
+            return arr.ToList();
+        }
+
+    }
+}
